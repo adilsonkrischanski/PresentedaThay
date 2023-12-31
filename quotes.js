53bb43144667527377c28d24187b56d0a49b1e25
Adilson Krischanski <adilson.krischanski@outlook.com>
2023-12-31 19:36:28 -0300

    arrumando frase

--- quotes.js
+++ quotes.js
@@ -1,8 +1,8 @@
 // quotes.js
 var quotes = [
-    { phrase: "01-01-2024. Hoje mais um ano começa, não posso te dar um abraço agora, mas saiba que quando 00h e foquetes estouram eu tava pensando em ti, que nesse nivi ano, possam surgir muitas oportunidades desafios e conquistas, vai nosso primeiro ano como casal, tudo bom pra gente, Te amo muito minha princesa e quero estar do teu lado em cada momento desse novo ano que se inicia, Feliz ano novo meu amor :)", date: "01-01-2024", color: "#d9534f" },
+    { phrase: "01-01-2024. Hoje mais um ano começa, não posso te dar o abraço que quero agora, mas saiba que quando deram 00h e foquetes estouravam nos céus  eu tava pensando em ti, que nesse novo ano, possam surgir muitas oportunidades desafios e conquistas, vai nosso primeiro ano como casal, tudo bom pra gente, Te amo muito minha princesa e quero estar do teu lado em cada momento desse novo ano que se inicia, Feliz ano novo minha princesa:)", date: "01-01-2024", color: "#d9534f" },
     { phrase: "02-01-2024. Hoje é dia 2, ja passou um dia, e hora de deixar as metas pra esse ano bem alinhadas e começar a trabalhar pra realizar elas, to aqui ctg minha princesa", date: "02-01-2023", color: "#5bc0de" },
-    { phrase: "Acredite que você pode e você está no meio do caminho.", date: "2023-01-03", color: "#5cb85c" },
+    { phrase: "03-01-2024. Acredite que você pode e você está no meio do caminho.", date: "03-01-2023", color: "#5cb85c" },
     
 ];
 
