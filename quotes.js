f19fd799c129b8e15c4861b29c1a151c3363a54e
Adilson Krischanski <adilson.krischanski@outlook.com>
2023-12-31 19:38:09 -0300

    add 31-12-23

--- quotes.js
+++ quotes.js
@@ -1,5 +1,6 @@
 // quotes.js
 var quotes = [
+    { phrase: "31-12-2023. Valido a Partir de 01-01-2024, mas ja pode olhar umas funcionalidades", date: "31-12-2023", color: "##5bc0de" },
     { phrase: "01-01-2024. Hoje mais um ano começa, não posso te dar o abraço que quero agora, mas saiba que quando deram 00h e foquetes estouravam nos céus  eu tava pensando em ti, que nesse novo ano, possam surgir muitas oportunidades desafios e conquistas, vai nosso primeiro ano como casal, tudo bom pra gente, Te amo muito minha princesa e quero estar do teu lado em cada momento desse novo ano que se inicia, Feliz ano novo minha princesa:)", date: "01-01-2024", color: "#d9534f" },
     { phrase: "02-01-2024. Hoje é dia 2, ja passou um dia, e hora de deixar as metas pra esse ano bem alinhadas e começar a trabalhar pra realizar elas, to aqui ctg minha princesa", date: "02-01-2023", color: "#5bc0de" },
     { phrase: "03-01-2024. Acredite que você pode e você está no meio do caminho.", date: "03-01-2023", color: "#5cb85c" },
